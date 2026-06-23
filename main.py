0b65b2af4df551644fc46026f7d04f7205d6f6f6
bbegmirzaev-source <bbegmirzaev@gmail.com>
2026-06-23 17:15:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -89,4 +89,19 @@ def run():
 
 t = Thread(target=run)
 t.start()
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+# Buni pollingdan oldin qo'shing
+t = Thread(target=run)
+t.start()
 
